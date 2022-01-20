6b7c6c113f120c459824d5300dd92b81a44eb168
Pierre-Olivier <sandozpierreolivier@yahoo.fr>
2022-01-20 09:55:54 +0100

    extra step 1 and 2 not working

--- docker-images/apache-reverse-proxy/templates/config-template.php
+++ docker-images/apache-reverse-proxy/templates/config-template.php
@@ -1,13 +1,31 @@
 <?php
     $dynamic_app = getenv('DYNAMIC_APP');
 	$static_app = getenv('STATIC_APP');
+	$static_app_premium = getenv('STATIC_APP_PREMIUM');
 ?>
 <VirtualHost *:80>
         ServerName demo.api.ch
+		
+		ProxyRequests Off
+		ProxyPreserveHost On
+		
+		Header add Set-Cookie "ROUTEID=.%{BALANCER_WORKER_ROUTE}e; path=/" env=BALANCER_ROUTE_CHANGED
+		
+		<Proxy balancer://dynamicApp>
+        BalancerMember 'http://<?php print "$dynamic_app"?>'
+		</Proxy>
+		
+		<Proxy balancer://staticApp>
+        BalancerMember 'http://<?php print "$static_app"?>' route=1
+        BalancerMember 'http://<?php print "$static_app_premium"?>' route=2
 
-        ProxyPass '/api/students/' 'http://<?php print "$dynamic_app"?>/'
-        ProxyPassReverse '/api/students/' 'http://<?php print "$dynamic_app"?>/'
+        ProxySet lbmethod=byrequests
+        ProxySet stickysession=ROUTEID
+		</Proxy>
 
-        ProxyPass '/' 'http://<?php print "$static_app"?>/'
-        ProxyPassReverse '/' 'http://<?php print "$static_app"?>/'
+        ProxyPass '/api/students/' 'balancer://dynamicApp/'
+        ProxyPassReverse '/api/students/' 'balancer://dynamicApp/'
+
+        ProxyPass '/' 'balancer://staticApp/'
+        ProxyPassReverse '/' 'balancer://staticApp/'
 </VirtualHost>
